ff2ea6f50cc6db757d307bc3ca8fa03f21a6e6f3
TehGaa <syosuasaputra1010@gmail.com>
2023-11-15 19:34:56 +0700

    feat: add linux router

--- custom/H04_2106750780.py
+++ custom/H04_2106750780.py
@@ -1,1 +1,24 @@
-print('hello world')
+from mininet.topo import Topo
+from mininet.node import Node
+
+class LinuxRouter(Node):
+    "A Node with IP forwarding enabled"
+
+    def config(self, **params):
+        super(LinuxRouter, self).config(**params)
+        # Enable forwarding on the router
+        self.cmd('sysctl net.ipv4.ip_forward=1')
+    
+    def terminate(self):
+        self.cmd('sysctl net.ipv4.ip_forward=0')
+        super(LinuxRouter, self).terminate()
+
+class MyTopo(Topo):
+    def build(self):
+        # number of router = 2
+        num_switch = 4
+        number_host_per_switch = [62, 30, 14, 6]
+        number_host_s1 = 0
+        number_host_s2 = 0
+        number_host_s3 = 0
+        number_host_s4 = 0
